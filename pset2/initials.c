cd41acce3d9bbe06dba9999343307f29c01ad88e
serenac26 <serenacheng99@hotmail.com>
2015-06-26 23:34:03 -0400

    initials

--- pset2/initials.c
+++ pset2/initials.c
@@ -1,3 +1,12 @@
+///////////////////////////////////////////////////
+
+    //initials.c 
+    //Prints the initials of an user input name.
+    //Author: Serena Cheng 
+    //Date: 6.26.15
+    
+///////////////////////////////////////////////////
+
 #include <stdio.h>
 #include <cs50.h>
 #include <ctype.h>
@@ -5,14 +14,17 @@
 
 int main(void)
 {
-  printf("Enter name please: ");
-  string name = GetString();
-  for (int i = 0, n = strlen(name); i < n; i++)
-  {
-    if (isalpha(name[i]) && (int)name[i-1] == 32)
+    string name = GetString();
+    if (isalpha(name[0]))
     {
-      printf("%c", toupper(name[i]));
+        printf("%c", toupper(name[0]));
     }
-  }
-  printf("\n");
+    for (int i = 0, n = strlen(name); i < n; i++)
+    {
+        if (isalpha(name[i]) && (int)name[i-1] == 32)
+        {
+            printf("%c", toupper(name[i]));
+        }
+    }
+    printf("\n");
 }
---
+++ pset2/vigenere.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(int argc, string argv[1])
+{
+    if (argc != 2)
+    {   
+        printf("Please enter a keyword in the command line argument\n");
+        return 1;    
+    }
+    else 
+    {
+        for (int a = 0; a < strlen(argv[1]); a++)
+        {
+            if (!isalpha(argv[1][a]))
+            {
+                printf("Only letters in keyword please\n");
+                return 1;
+            }
+        }
+        string message = GetString();
+        string k = argv[1];
+        for (int j = 0, m = strlen(message); j < m; j++)
+        {
+            int i = (j%strlen(k));
+            if (message[j] > 96 && message[j] < 123)
+            {
+                int lc = ((int)message[j]-96+(int)toupper(k[i])-65)%26;
+                if (lc == 0)
+                {
+                    printf("z");
+                }
+                else
+                {
+                printf("%c", lc+96);                    
+                }                                  
+            }
+            else if (message[j] > 64 && message[j] < 91)
+            {
+                int uc = ((int)message[j]-64+(int)toupper(k[i])-65)%26;
+                if (uc == 0)
+                {
+                    printf("z");
+                }
+                else
+                {
+                printf("%c", uc+64);
+                }        
+            }
+            else 
+            {
+                printf("%c", message[j]);
+            }
+        }     
+    }
+    printf("\n");
+    return 0;
+}
+
